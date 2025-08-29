98165d2a85bfcba9e83d1e7b2fa2311c11595d13
Akash Danda <akashdanda@Akashs-MacBook-Pro-2.local>
2025-08-29 09:44:45 -0500

    encoder block implementation

--- transformer.py
+++ transformer.py
@@ -140,9 +140,19 @@ class Residual_Connection(nn.Module):
 class Encoder_Block(nn.Module):
     #order: input_embedding positional embeddimg, multhead self attention, residual connection & layer norm, feed forward, res connect and layer norm again
 
-    def __init__(self,self_attention_block: MultiHead_Attention, feed_forward_block: MultiHead_Attention, dropout: float) -> None:
-        super().__init__
+    def __init__(self,self_attention_block: MultiHead_Attention, feed_forward_block: Feed_Forward, dropout: float) -> None:
+        super().__init__()
         self.self_attention_block = self_attention_block
         self.feed_forward_block = feed_forward_block
         self.residual_connections = nn.ModuleList([Residual_Connection(dropout) for _ in range(2)])
+    
+
+    def forward(self, x, src_mask):
+        #run through multihead then residual
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x, src_mask))
+        #run throguh feed forward then residual
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
+
     
